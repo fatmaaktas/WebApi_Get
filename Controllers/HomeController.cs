baa6d5033680d983409d0e7b78466e1977a2b697
ffatmaktass <fatmaaaktass10@gmail.com>
2023-12-27 15:55:38 +0300

    Web_Api_Get created

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using HelloWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebApi.Controllers
+{
+
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var result = new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core"
+            };
+            
+            return Ok(result);
+        }
+    }
+}
